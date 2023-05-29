212abeef76d9551448f825c95f3b432b5219401f
engkoskostaman97 <engkoskostaman759@gmail.com>
2023-05-29 16:04:19 +0700

    basic javaspringboot connect mysql

---
+++ src/main/java/com/crudsimplesjpa/belajarjavanew/BelajarjavanewApplication.java
@@ -0,0 +1,13 @@
+package com.crudsimplesjpa.belajarjavanew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarjavanewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarjavanewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crudsimplesjpa/belajarjavanew/BelajarjavanewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudsimplesjpa.belajarjavanew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarjavanewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
